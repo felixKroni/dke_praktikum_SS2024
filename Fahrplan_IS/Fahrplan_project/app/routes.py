724406fd68cfd2d6fa2c40b0d19bd8a610f4522c
felixKroni <felixkronsteiner@gmx.at>
2024-03-28 19:43:35 +0100

    quicksave

---
+++ Fahrplan_IS/Fahrplan_project/app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, flash, redirect
+
+from app import app
+from app.forms.loginForm import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Super Admin'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
