50330486578bd0ec0760b90dd44d837e1e12adca
HeiPat <127215359+HeiPat@users.noreply.github.com>
2024-05-22 16:15:57 +0200

    V1.0

--- Fahrplan_IS/Fahrplan_project/app/routes.py
+++ Fahrplan_IS/Fahrplan_project/app/routes.py
@@ -358,21 +358,21 @@ def confirmFahrplan(fahrplanId):
 ###########################
 # REST requests
 def get_strecken():
-    response = requests.get("http://127.0.0.1:5001/api/strecken")
+    response = requests.get("http://localhost:5001/api/strecken")
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
 def get_all_haltepunkte_of_strecke(strecke_name):
-    response = requests.get("http://127.0.0.1:5001/api/strecken/" + str(strecke_name))
+    response = requests.get("http://localhost:5001/api/strecken/" + str(strecke_name))
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
 def get_haltepunkte_names(strecke_name):
-    response = requests.get("http://127.0.0.1:5001/api/strecken/" + str(strecke_name))
+    response = requests.get("http://localhost:5001/api/strecken/" + str(strecke_name))
     if response.status_code == 200:
         abschnitte = response.json().get('abschnitte', [])
         names = [abschnitt['startbahnhof_id'] for abschnitt in abschnitte]
